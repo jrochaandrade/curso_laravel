8562c647d2ee858b5e97a069bbe92e2b72a8e0fa
jrochaandrade <jrochaandrade@gmail.com>
2023-04-04 23:07:11 -0400

    Colocando informações na tabela
    Usando o seed para colocar informações na tabela para testes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+        //$this->call(UsersTableSeeder::class);
+
+        //\App\Models\User::factory(10)->create();
+
+        //O código abaixou eu criei para testar.
+
+        \App\Models\User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => 'testetetete',
+        ]);
+
+       // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+         //    'email' => 'test@example.com',
         // ]);
     }
 }
